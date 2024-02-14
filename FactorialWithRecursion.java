d42d925a632b7a1c894130ad0ded9ba02053bf40
Athulya Saji <144237154+athulyasaji05@users.noreply.github.com>
2024-02-14 21:18:46 +0530

    Add files via upload

---
+++ FactorialWithRecursion.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class FactorialWithRecursion {
+    static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+    public static void main(String[] args) {
+        try {
+            DataInputStream dis = new DataInputStream(System.in);
+            System.out.print("Enter a number : ");
+            int inputNumber = Integer.parseInt(dis.readLine());
+            int result = factorial(inputNumber);
+            System.out.println("Factorial of " + inputNumber + " is: " + result);
+        } catch (Exception e) {
+           System.out.println(e);
+        }
+    }
+}
